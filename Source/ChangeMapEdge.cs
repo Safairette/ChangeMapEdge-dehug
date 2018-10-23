81127ab936651039f7894b8770c33e7d00d96758
Kapitan Oczywisty <44417092+KapitanOczywisty@users.noreply.github.com>
2018-10-24 00:49:16 +0200

    Initial version

---
+++ Source/ChangeMapEdge.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Verse;
+using UnityEngine;
+using RimWorld;
+using HugsLib;
+using HugsLib.Settings;
+
+namespace ChangeMapEdge
+{
+    public class ChangeMapEdge : ModBase
+    {
+
+        public static ChangeMapEdge Instance { get; private set; }
+
+        public ChangeMapEdge()
+        {
+            Instance = this;
+        }
+        public override string ModIdentifier
+        {
+            get { return "ChangeMapEdge"; }
+        }
+        private SettingHandle<int> noBuildLimit;
+        private SettingHandle<int> noZoneLimit;
+        public override void DefsLoaded()
+        {
+            noBuildLimit = Settings.GetHandle<int>("noBuildEdge", "No-build edge size", "Restrict building to X tiles from edge. Game default: 10", 10, Validators.IntRangeValidator(0, 20));
+            noZoneLimit = Settings.GetHandle<int>("noZoneEdge", "No-zone edge size", "Restrict zones to X tiles from edge. Game default: 5", 5, Validators.IntRangeValidator(0, 10));
+        }
+
+        public int GetNoBuildLimit() => noBuildLimit.Value;
+        public int GetNoZoneLimit() => noZoneLimit.Value;
+
+    }
+}
---
+++ Source/ChangeMapEdgeHarmony.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Verse;
+using Harmony;
+using System.Reflection;
+using RimWorld;
+using UnityEngine;
+
+namespace ChangeMapEdge
+{
+    [StaticConstructorOnStartup]
+    static class ChangeMapEdgeHarmony
+    {
+        static ChangeMapEdgeHarmony()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.kapitanoczywisty.changemapedge");
+
+            MethodInfo nobuild_targetmethod = AccessTools.Method(typeof(Verse.GenDraw), "DrawNoBuildEdgeLines");
+            HarmonyMethod nobuild_prefixmethod = new HarmonyMethod(typeof(ChangeMapEdgeHarmony).GetMethod("DrawNoBuildEdgeLines_Prefix"));
+            harmony.Patch(nobuild_targetmethod, nobuild_prefixmethod, null);
+
+            MethodInfo nozone_targetmethod = AccessTools.Method(typeof(Verse.GenDraw), "DrawNoZoneEdgeLines");
+            HarmonyMethod nozone_prefixmethod = new HarmonyMethod(typeof(ChangeMapEdgeHarmony).GetMethod("DrawNoZoneEdgeLines_Prefix"));
+            harmony.Patch(nozone_targetmethod, nozone_prefixmethod, null);
+
+            MethodInfo innobuild_targetmethod = AccessTools.Method(typeof(Verse.GenGrid), "InNoBuildEdgeArea");
+            HarmonyMethod innobuild_postfixmethod = new HarmonyMethod(typeof(ChangeMapEdgeHarmony).GetMethod("InNoBuildEdgeArea_Postfix"));
+            harmony.Patch(innobuild_targetmethod, null, innobuild_postfixmethod);
+
+            MethodInfo innozone_targetmethod = AccessTools.Method(typeof(Verse.GenGrid), "InNoZoneEdgeArea");
+            HarmonyMethod innozone_postfixmethod = new HarmonyMethod(typeof(ChangeMapEdgeHarmony).GetMethod("InNoZoneEdgeArea_Postfix"));
+            harmony.Patch(innozone_targetmethod, null, innozone_postfixmethod);
+        }
+
+        static Material LineMatMetaOverlay = MaterialPool.MatFrom(GenDraw.LineTexPath, ShaderDatabase.MetaOverlay);
+
+        private static void DrawMapEdgeLines(int edgeDist)
+        {
+            float y = AltitudeLayer.MetaOverlays.AltitudeFor();
+            IntVec3 size = Find.CurrentMap.Size;
+            Vector3 vector = new Vector3((float)edgeDist, y, (float)edgeDist);
+            Vector3 vector2 = new Vector3((float)edgeDist, y, (float)(size.z - edgeDist));
+            Vector3 vector3 = new Vector3((float)(size.x - edgeDist), y, (float)(size.z - edgeDist));
+            Vector3 vector4 = new Vector3((float)(size.x - edgeDist), y, (float)edgeDist);
+            GenDraw.DrawLineBetween(vector, vector2, ChangeMapEdgeHarmony.LineMatMetaOverlay);
+            GenDraw.DrawLineBetween(vector2, vector3, ChangeMapEdgeHarmony.LineMatMetaOverlay);
+            GenDraw.DrawLineBetween(vector3, vector4, ChangeMapEdgeHarmony.LineMatMetaOverlay);
+            GenDraw.DrawLineBetween(vector4, vector, ChangeMapEdgeHarmony.LineMatMetaOverlay);
+        }
+
+        public static bool DrawNoBuildEdgeLines_Prefix()
+        {
+            int limit = ChangeMapEdge.Instance.GetNoBuildLimit();
+            if (limit > 0)
+                ChangeMapEdgeHarmony.DrawMapEdgeLines(limit);
+            return false;
+        }
+        public static bool DrawNoZoneEdgeLines_Prefix()
+        {
+            int limit = ChangeMapEdge.Instance.GetNoZoneLimit();
+            if (limit > 0)
+                ChangeMapEdgeHarmony.DrawMapEdgeLines(limit);
+            return false;
+        }
+        public static void InNoBuildEdgeArea_Postfix(this IntVec3 c, Map map, ref bool __result)
+        {
+            __result = c.CloseToEdge(map, ChangeMapEdge.Instance.GetNoBuildLimit());
+        }
+        public static void InNoZoneEdgeArea_Postfix(this IntVec3 c, Map map, ref bool __result)
+        {
+            __result = c.CloseToEdge(map, ChangeMapEdge.Instance.GetNoZoneLimit());
+        }
+    }
+}
